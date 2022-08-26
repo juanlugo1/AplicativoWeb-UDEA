13e983281b0a0098aca3a2ef0c00210a777252a5
WillianZambrano <106875357+WillianZambrano@users.noreply.github.com>
2022-08-25 19:22:29 -0500

    Creacion d e proyecto Visual Code
    Se genera el nuevo proyecto desde Visual Code

---
+++ apiingresosyegresos/src/main/java/proyectobassicprogrammers/apiingresosyegresos/ApiingresosyegresosApplication.java
@@ -0,0 +1,13 @@
+package proyectobassicprogrammers.apiingresosyegresos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiingresosyegresosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiingresosyegresosApplication.class, args);
+	}
+
+}
